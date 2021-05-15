d1d47d5059006cb86527bb5f53d667f494eaa521
Mateus Mazzei Anzolin <mateus.mazzei14@gmail.com>
2021-05-15 18:10:57 -0300

    esqueci o arquivo

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$db_name = "teste_jobzeira";
+
+$connect = mysqli_connect($servername,$username,$password,$db_name);
+mysqli_set_charset($connect,"utf8");
+
+if(mysqli_connect_error()):
+    echo "Erro na conexao: ".mysqli_connect_error();
+endif;
+?>
