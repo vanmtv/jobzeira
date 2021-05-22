b77989301d9e4d3f88d44b29cd167551f135d2a4
Mateus Mazzei Anzolin <mateus.mazzei14@gmail.com>
2021-05-21 22:23:29 -0300

    Alguns updates

--- db_connect.php
+++ db_connect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "password";
-$db_name = "teste_jobzeira";
+$db_name = "jobzeira";
 
 $connect = mysqli_connect($servername,$username,$password,$db_name);
 mysqli_set_charset($connect,"utf8");
