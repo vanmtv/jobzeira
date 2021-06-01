cfdc8dfa60f4c0b0ddd8141e3398afaff80b931c
Mateus Mazzei Anzolin <mateus.mazzei14@gmail.com>
2021-06-01 01:40:01 -0300

    Editar anúncios e página de meus anúncios

--- meus_itens.php
+++ meus_itens.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilo/base.css">
-    <link rel="stylesheet" href="estilo/anuncio.css">
+    <link rel="stylesheet" href="estilo/meus_itens.css">
     <link rel="stylesheet" href="estilo/font-awesome/css/font-awesome.min.css">
     <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="scripts/meus_itens.js"></script>
@@ -27,6 +27,39 @@
     </header>
     <div class="content">
         <h1 class="titulo">Meus anúncios</h1>
+        <div class="table">
+            <table>
+                <?php 
+                $sql = "SELECT `anuncio_id`, `titulo`,`valor`, `descricao` FROM `anuncios` WHERE `profissional_id` = " . $_GET['id'];
+                $resultado = mysqli_query($connect,$sql);
+                $tableHead = '
+                    <tr class="header">
+                        <th>Título</th>
+                        <th>Preço</th>
+                        <th>Descrição</th>
+                    </tr>';
+                $count = 0;
+                $tableBoady = '';
+
+                while($item = mysqli_fetch_array($resultado)){
+                    if($count % 2 == 0) $class = 'zebraOff';
+                    else $class = 'zebraOn';
+                    $count++;
+                    $tableBoady .= '
+                    <tr class="'.$class.'">
+                        <td><a href="'.'anuncio.php?item='.$item[0].'">'.$item[1].'</a></td>
+                        <td><a href="'.'anuncio.php?item='.$item[0].'">'.$item[2].'</a></td>
+                        <td><a href="'.'anuncio.php?item='.$item[0].'">'.$item[3].'</a></td>
+                    </tr>';
+                }
+                echo $tableHead;
+                if($count > 0)
+                    echo $tableBoady;
+                else 
+                    echo '<tr class="zebraOff"><td colspan = 3> Nenhum item encontrado</tr>';
+                ?>
+            </table>
+        </div>
     </div>
     <footer>
         <!--div><img src="imagens/logo.svg" alt="Logo" class="footer-logo"></div-->
