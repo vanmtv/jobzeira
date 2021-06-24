4a08a6c53c8ba2923e0b5e2822f0b13c072a809f
Mateus Mazzei Anzolin <mateus.mazzei14@gmail.com>
2021-06-24 19:54:04 -0300

    esqueci do db_connect

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$db_name = "jobzeira";
+$servername = "us-cdbr-east-04.cleardb.com";
+$username = "b1b365650f4624";
+$password = "7c0cb235";
+$db_name = "heroku_f84857fca49b4f1";
 
 $connect = mysqli_connect($servername,$username,$password, $db_name);
 mysqli_set_charset($connect,"utf8");
